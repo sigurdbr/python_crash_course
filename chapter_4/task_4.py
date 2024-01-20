14b516dbcce144b4a303989ee1822933ad9722c8
Sigurd Rønning <sigurd.bakaas@outlook.com>
2024-01-20 19:15:08 +0100

    Added task_4.py

---
+++ chapter_4/task_4.py
@@ -0,0 +1,8 @@
+#4-13
+simple_foods = ("Hamburger", "Kebab", "Hot Dog", "Pizza", "Fish n Chips")
+for food in simple_foods:
+    print(food)
+
+simple_foods = ("Stew", "Baguette", "Hot Dog", "Pizza", "Fish n Chips")
+for food in simple_foods:
+    print(food)
