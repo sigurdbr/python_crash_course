5cac5214cfb429ca63aeac11562583428a1aff7b
Sigurd Rønning <sigurd.bakaas@outlook.com>
2024-01-26 16:54:13 +0100

    Adding chapter_6 folder and task_1.py

---
+++ chapter_6/task_1.py
@@ -0,0 +1,22 @@
+#6-1
+person =  {"first_name": "Sigurd", 
+           "last_name": "Rønning", 
+           "age": 25, 
+           "city": "Stange",
+           }
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
+
+#6-2
+favorite_numbers = {"sigurd": 6,
+                    "aivi": 28,
+                    "sander": 16,
+                    "jørgen": 5,
+                    "oliver": 5,
+                    }
+
+for key, value in favorite_numbers.items():
+    print(f"{key.title()}s favorite number is {value}")
