937dc01a1751faceb48f44a5f6cd13fbe0f39428
Sigurd Rønning <sigurd.bakaas@outlook.com>
2024-03-05 23:04:22 +0100

    Adding task_3.py. File is unfinished.

---
+++ chapter_6/task_3.py
@@ -0,0 +1,26 @@
+#6-7
+
+person_1 = {
+    "first_name": "sigurd",
+    "last_name": "rønning",
+    "age": 25,
+    "city": "stange",
+}
+
+person_2 = {
+    "first_name": "aivi",
+    "last_name": "falkum",
+    "age": 23,
+    "city": "stange",
+}
+
+person_3 = {
+    "first_name": "jørgen",
+    "last_name": "rønning",
+    "age": 26,
+    "city": "oslo",
+}
+
+people = [person_1, person_2, person_3]
+
+print(people)
